78318d380dfa3fd6540cf08423cdeaf014a440ba
cbs001 <121211685+cbs001@users.noreply.github.com>
2024-12-07 22:15:54 +0800

    v3

--- data.js
+++ data.js
@@ -193,17 +193,57 @@ function new_judge_line() {
 	};
 }
 
+
+function large_json_stringify(obj, write) {
+	if (obj === null) write("null");
+	else if (obj === undefined) write("undefined");
+	else if (typeof obj === "object") {
+		let ans = [];
+		if (Array.isArray(obj)) {
+			write("[");
+			for (let i = 0; i < obj.length; i++) {
+				if (i) write(",");
+				large_json_stringify(obj[i], write);
+			}
+			write("]");
+		} else {
+			write("{");
+			let f = 1;
+			for (key in obj) {
+				if (obj.hasOwnProperty(key)) {
+					if (f == 1) f = 0;
+					else write(",");
+					write("\"" + key + "\":");
+					large_json_stringify(obj[key], write);
+				}
+			}
+			write("}");
+		}
+		return ans;
+	} else {
+		if (typeof obj == "string") write("\"" + obj + "\"");
+		else write(obj.toString());
+	}
+}
 // 加载与保存
 // ============================================================================================
 async function FileDownload(content, filename) {
 	const eleLink = document.createElement('a');
 	eleLink.download = filename;
 	eleLink.style.display = 'none';
-	const blob = new Blob([content]);
+	const blob = new Blob(content);
 	eleLink.href = URL.createObjectURL(blob);
 	document.body.appendChild(eleLink);
 	eleLink.click();
 	document.body.removeChild(eleLink);
+}
+function fix() {
+	for (let i = 0; i < all_data.judgeLineList.length; i++) {
+		let line = all_data.judgeLineList[i];
+		if (line.extended == undefined) line.extended = {};
+		if (line.anchor == undefined) line.anchor = [0.5, 0.5];
+		if (line.bpmfactor == undefined) line.bpmfactor = 1.0;
+	}
 }
 $("m-load").addEventListener('click', () => {
 	var fileInput = document.createElement('input');
@@ -216,42 +256,46 @@ $("m-load").addEventListener('click', () => {
 	fileInput.onchange = function (e) {
 		var files = e.target.files;
 		if (files.length) {
-			var reader = new FileReader();
-			reader.onload = function (e) {
-				// console.log(e);
-				try {
-					all_data = JSON.parse(e.target.result);
-				} catch (error) {
-					let a = e.target.result.split('\n'), n = parseInt(a[0]); // a 表示每一行的字符串
-					all_data = new_empty_data();
-					all_data.judgeLineList.push(new_judge_line());
-					for (let i = 1; i <= n; i++) {
-						let tmp = a[i].split(' ');
-						let type = parseInt(tmp[0]);
-						let st = parseInt(tmp[1]) / 66.6 * (bpm / 60);
-						let speed = parseInt(tmp[2]);
-						let x = parseInt(tmp[tmp.length - 1]);
-						let ed = type === 3 ? st + Math.floor(parseInt(tmp[3]) / 66.6 * (bpm / 60)) : st;
-						let note = {
-							type: type,
-							startTime: [Math.floor(st), Math.round((st % 1) * 16), 16],
-							positionX: (x - 1) * (1350 / shu) - 675,
-							endTime: [Math.floor(ed), Math.round((ed % 1) * 16), 16]
-						};
-						all_data.judgeLineList[0].notes.push(note);
+			console.log("加载成功, 类型：" + files[0].type);
+
+			if (files[0].type.includes("audio")) {
+				$("music-player").src = URL.createObjectURL(files[0]);
+			} else {
+				var reader = new FileReader();
+				reader.onload = function (e) {
+					try {
+						all_data = JSON.parse(e.target.result);
+					} catch (error) {
+						let a = e.target.result.split('\n'), n = parseInt(a[0]); // a 表示每一行的字符串
+						all_data = new_empty_data();
+						all_data.judgeLineList.push(new_judge_line());
+						for (let i = 1; i <= n; i++) {
+							let tmp = a[i].split(' ');
+							let type = parseInt(tmp[0]);
+							let st = parseInt(tmp[1]) / 66.6 * (bpm / 60);
+							let speed = parseInt(tmp[2]);
+							let x = parseInt(tmp[tmp.length - 1]);
+							let ed = type === 3 ? st + Math.floor(parseInt(tmp[3]) / 66.6 * (bpm / 60)) : st;
+							let note = {
+								type: type,
+								startTime: [Math.floor(st), Math.round((st % 1) * 16), 16],
+								positionX: (x - 1) * (1350 / shu) - 675,
+								endTime: [Math.floor(ed), Math.round((ed % 1) * 16), 16]
+							};
+							all_data.judgeLineList[0].notes.push(note);
+						}
 					}
-				}
-				/*
-				notes，evs 都是 all_data 中的引用
-				*/
-				notes = all_data.judgeLineList[0].notes;
-				notecontrol.update();
-				evs = all_data.judgeLineList[0].eventLayers;
-				$("lines").value = 0;
-				selection = [];
-				settingscontrol.load(); // 加载基本设置
-			};
-			reader.readAsText(files[0]);
+					/*
+					notes，evs 都是 all_data 中的引用
+					*/
+					$("lines").value = 0;
+					change_line();
+					fix();
+					bpm = all_data.BPMList[0].bpm;
+					settingscontrol.load(); // 加载基本设置
+				};
+				reader.readAsText(files[0]);
+			}
 		}
 	};
 });
@@ -259,12 +303,31 @@ $("m-save").addEventListener('click', () => {
 	if (in_download == 0) {
 		in_download = 1;
 		$("downloading").style.display = "block";
-		for (let i = 0; i < all_data.judgeLineList.length; i++) {
-			all_data.judgeLineList[i].numOfNotes = all_data.judgeLineList[i].notes.length;
-		}
-		FileDownload(JSON.stringify(all_data), "chart.json");
-		in_download = 0;
-		$("downloading").style.display = "none";
+		setTimeout(() => {
+			for (let i = 0; i < all_data.judgeLineList.length; i++) {
+				all_data.judgeLineList[i].numOfNotes = all_data.judgeLineList[i].notes.length;
+			}
+
+			if (window.showSaveFilePicker == undefined) {
+				FileDownload(JSON.stringify(all_data), "chart.json");
+			} else {
+				// 流式保存
+				window.showSaveFilePicker({ suggestedName: "chart.json" })
+					.then(handle => handle.createWritable())
+					.then(stream => {
+						// console.log(stream);
+						let cache = "";
+						large_json_stringify(all_data, (s) => {
+							if (cache.length + s.length <= 1e7) cache += s;
+							else stream.write(cache), cache = s;
+						});
+						stream.write(cache);
+						stream.close();
+					});
+			}
+			in_download = 0;
+			$("downloading").style.display = "none";
+		}, 0);
 	}
 });
 $("m-save2").addEventListener('click', () => {
