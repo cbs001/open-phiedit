cced7e7ff77528a885ed237e9a8dcabaca69e814
cbs001 <121211685+cbs001@users.noreply.github.com>
2025-01-15 13:44:22 +0800

    v4

--- data.js
+++ data.js
@@ -243,7 +243,62 @@ function fix() {
 		if (line.extended == undefined) line.extended = {};
 		if (line.anchor == undefined) line.anchor = [0.5, 0.5];
 		if (line.bpmfactor == undefined) line.bpmfactor = 1.0;
+		if (line.notes == undefined) line.notes = [];
 	}
+}
+function large_json_parse(e) {
+	if (e.str == undefined) return large_json_parse({ str: e, i: 0 });
+	while (e.str[e.i] == 32 || e.str[e.i] == 9 || e.str[e.i] == 10 || e.str[e.i] == 13) e.i++;
+	if (e.str[e.i] === 123) { // object
+		e.i++;
+		var result = {};
+		while (e.str[e.i] !== 125) {
+			if (e.i >= e.str.length) throw new Error("JSON 格式错误：大括号未闭合");
+			var key = large_json_parse(e);
+			e.i++;
+			var value = large_json_parse(e);
+			result[key] = value;
+			if (e.str[e.i] === 44) e.i++; // 逗号
+		}
+		e.i++;
+		return result;
+	} else if (e.str[e.i] === 91) { // array
+		e.i++;
+		var result = [];
+		while (e.str[e.i] !== 93) {
+			if (e.i >= e.str.length) throw new Error("JSON 格式错误：中括号未闭合");
+			result.push(large_json_parse(e));
+			if (e.str[e.i] === 44) e.i++;  // 逗号
+		}
+		e.i++;
+		return result;
+	} else if (e.str[e.i] === 110) {  // null
+		e.i += 4;
+		return null;
+	} else if (e.str[e.i] === 116) { // true
+		e.i += 4;
+		return true;
+	} else if (e.str[e.i] === 102) { // false
+		e.i += 5;
+		return false;
+	} else if (e.str[e.i] === 34) { // string
+		var result = "";
+		e.i++; // 跳过双引号
+		while (e.str[e.i] != 34) result += String.fromCharCode(e.str[e.i++])
+		e.i++;
+		return result;
+	} else { // number
+		var numStr = "";
+		while (45 <= e.str[e.i] && e.str[e.i] <= 57) numStr += String.fromCharCode(e.str[e.i++]);
+		return parseFloat(numStr);
+	}
+}
+function toa(s) {
+	let a = new Uint8Array(s.length);
+	for (let i = 0; i < s.length; i++) {
+		a[i] = s.charCodeAt(i);
+	}
+	return a
 }
 $("m-load").addEventListener('click', () => {
 	var fileInput = document.createElement('input');
@@ -257,44 +312,80 @@ $("m-load").addEventListener('click', () => {
 		var files = e.target.files;
 		if (files.length) {
 			console.log("加载成功, 类型：" + files[0].type);
-
 			if (files[0].type.includes("audio") || files[0].type == "video/ogg") {
 				$("music-player").src = URL.createObjectURL(files[0]);
 			} else {
-				var reader = new FileReader();
-				reader.onload = function (e) {
-					try {
-						all_data = JSON.parse(e.target.result);
-					} catch (error) {
-						let a = e.target.result.split('\n'), n = parseInt(a[0]); // a 表示每一行的字符串
-						all_data = new_empty_data();
-						all_data.judgeLineList.push(new_judge_line());
-						for (let i = 1; i <= n; i++) {
-							let tmp = a[i].split(' ');
-							let type = parseInt(tmp[0]);
-							let st = parseInt(tmp[1]) / 66.6 * (bpm / 60);
-							let speed = parseInt(tmp[2]);
-							let x = parseInt(tmp[tmp.length - 1]);
-							let ed = type === 3 ? st + Math.floor(parseInt(tmp[3]) / 66.6 * (bpm / 60)) : st;
-							let note = {
-								type: type,
-								startTime: [Math.floor(st), Math.round((st % 1) * 16), 16],
-								positionX: (x - 1) * (1350 / shu) - 675,
-								endTime: [Math.floor(ed), Math.round((ed % 1) * 16), 16]
-							};
-							all_data.judgeLineList[0].notes.push(note);
-						}
-					}
+				function init() {
 					/*
-					notes，evs 都是 all_data 中的引用
-					*/
+				notes，evs 都是 all_data 中的引用
+				*/
 					$("lines").value = 0;
 					change_line();
 					fix();
-					bpm = all_data.BPMList[0].bpm;
+					bpm = all_data.BPMList;
 					settingscontrol.load(); // 加载基本设置
-				};
-				reader.readAsText(files[0]);
+
+					let sum = 0;
+					for (let i = 0; i < all_data.judgeLineList.length; i++)
+						if (all_data.judgeLineList[i].notes)
+							sum += all_data.judgeLineList[i].notes.length;
+					if (sum >= 5000) {
+						settingscontrol.settings.player = 0;
+						window.alert("谱面物量较多，已自动关闭预览功能");
+					}
+				}
+				var reader = new FileReader();
+				if (files[0].size > 500 * 1024 * 1024) {
+					if (window.prompt("谱面文件超过 500 MB，继续读取可能出现问题，是否继续？（继续输入 Y）").trim() == "Y") {
+						let st = performance.now();
+						reader.onload = function (e) {
+							let data = new Uint8Array(e.target.result)
+							try {
+								// console.log(data);
+								all_data = large_json_parse(data);
+								console.log("用时：", performance.now() - st);
+								init();
+							} catch (error) {
+								window.alert("解析错误");
+							}
+						};
+						reader.readAsArrayBuffer(files[0]);
+					}
+				} else {
+					reader.onload = function (e) {
+						try {
+							all_data = JSON.parse(e.target.result);
+							init();
+						} catch (error) {
+							try {
+								console.log(error);
+								let a = e.target.result.split('\n'), n = parseInt(a[0]); // a 表示每一行的字符串
+								all_data = new_empty_data();
+								all_data.judgeLineList.push(new_judge_line());
+								for (let i = 1; i <= n; i++) {
+									let tmp = a[i].split(' ');
+									let type = parseInt(tmp[0]);
+									let st = parseInt(tmp[1]) / 66.6 * (bpm / 60);
+									let speed = parseInt(tmp[2]);
+									let x = parseInt(tmp[tmp.length - 1]);
+									let ed = type === 3 ? st + Math.floor(parseInt(tmp[3]) / 66.6 * (bpm / 60)) : st;
+									let note = {
+										type: type,
+										startTime: [Math.floor(st), Math.round((st % 1) * 16), 16],
+										positionX: (x - 1) * (1350 / shu) - 675,
+										endTime: [Math.floor(ed), Math.round((ed % 1) * 16), 16]
+									};
+									all_data.judgeLineList[0].notes.push(note);
+								}
+								init();
+							} catch (error) {
+								window.alert("解析错误");
+							}
+						}
+					};
+					reader.readAsText(files[0]);
+				}
+
 			}
 		}
 	};
@@ -305,7 +396,7 @@ $("m-save").addEventListener('click', () => {
 		$("downloading").style.display = "block";
 		setTimeout(() => {
 			for (let i = 0; i < all_data.judgeLineList.length; i++) {
-				if(all_data.judgeLineList[i].notes == undefined) all_data.judgeLineList[i].numOfNotes = 0;
+				if (all_data.judgeLineList[i].notes == undefined) all_data.judgeLineList[i].numOfNotes = 0;
 				else all_data.judgeLineList[i].numOfNotes = all_data.judgeLineList[i].notes.length;
 			}
 
